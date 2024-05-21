8408541d0992956e06bd28c469cad4292f66490a
超 张 <2074119880@qq.com>
2024-05-21 14:19:35 +0800

    echarts大屏幕配制

--- vue-element-admin/src/components/Charts/Keyboard.vue
+++ vue-element-admin/src/components/Charts/Keyboard.vue
@@ -4,7 +4,117 @@
 
 <script>
 import echarts from 'echarts'
-import resize from './mixins/resize'
+import * as echarts from 'echarts';
+
+var chartDom = document.getElementById('main');
+var myChart = echarts.init(chartDom);
+var option;
+
+const gaugeData = [
+  {
+    value: 20,
+    name: 'Perfect',
+    title: {
+      offsetCenter: ['0%', '-30%']
+    },
+    detail: {
+      valueAnimation: true,
+      offsetCenter: ['0%', '-20%']
+    }
+  },
+  {
+    value: 40,
+    name: 'Good',
+    title: {
+      offsetCenter: ['0%', '0%']
+    },
+    detail: {
+      valueAnimation: true,
+      offsetCenter: ['0%', '10%']
+    }
+  },
+  {
+    value: 60,
+    name: 'Commonly',
+    title: {
+      offsetCenter: ['0%', '30%']
+    },
+    detail: {
+      valueAnimation: true,
+      offsetCenter: ['0%', '40%']
+    }
+  }
+];
+option = {
+  series: [
+    {
+      type: 'gauge',
+      startAngle: 90,
+      endAngle: -270,
+      pointer: {
+        show: false
+      },
+      progress: {
+        show: true,
+        overlap: false,
+        roundCap: true,
+        clip: false,
+        itemStyle: {
+          borderWidth: 1,
+          borderColor: '#464646'
+        }
+      },
+      axisLine: {
+        lineStyle: {
+          width: 40
+        }
+      },
+      splitLine: {
+        show: false,
+        distance: 0,
+        length: 10
+      },
+      axisTick: {
+        show: false
+      },
+      axisLabel: {
+        show: false,
+        distance: 50
+      },
+      data: gaugeData,
+      title: {
+        fontSize: 14
+      },
+      detail: {
+        width: 50,
+        height: 14,
+        fontSize: 14,
+        color: 'inherit',
+        borderColor: 'inherit',
+        borderRadius: 20,
+        borderWidth: 1,
+        formatter: '{value}%'
+      }
+    }
+  ]
+};
+setInterval(function () {
+  gaugeData[0].value = +(Math.random() * 100).toFixed(2);
+  gaugeData[1].value = +(Math.random() * 100).toFixed(2);
+  gaugeData[2].value = +(Math.random() * 100).toFixed(2);
+  myChart.setOption({
+    series: [
+      {
+        data: gaugeData,
+        pointer: {
+          show: false
+        }
+      }
+    ]
+  });
+}, 2000);
+
+option && myChart.setOption(option);
 
 export default {
   mixins: [resize],
